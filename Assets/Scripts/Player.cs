26d944bb4c7a87f975e08b1c79c98f44c29bd086
Selena Qian <sq22@duke.edu>
2020-12-29 04:47:13 -0500

    started adding database connection

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Text;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.5f;
+
+    //private Rigidbody2D _rigidBody2D;
+    private Vector2 _movement;
+    private PlayerData _playerData;
+
+    void Start()
+    {
+        //_rigidBody2D = GetComponent<Rigidbody2D>();
+        _playerData = new PlayerData();
+        _playerData.username = "test";
+        StartCoroutine(Download(_playerData.username, result => {
+          Debug.Log(result);
+        }));
+    }
+
+    void Update()
+    {
+        // Mouse and keyboard input logic here ...
+    }
+
+    void FixedUpdate() {
+        // Physics related updates here ...
+    }
+
+    IEnumerator Download(string id, System.Action<PlayerData> callback = null)
+    {
+      using (UnityWebRequest request = UnityWebRequest.Get("https://webhooks.mongodb-realm.com/api/client/v2.0/app/covidwarrior-xhivn/service/CovidWarriorInfo/incoming_webhook/getUser?username=" + id))
+      {
+          yield return request.SendWebRequest();
+
+          if (request.isNetworkError || request.isHttpError)
+          {
+              Debug.Log(request.error);
+              if (callback != null)
+              {
+                  callback.Invoke(null);
+              }
+          }
+          else
+          {
+              if (callback != null)
+              {
+                  callback.Invoke(PlayerData.Parse(request.downloadHandler.text));
+              }
+          }
+      }
+  }
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerData
+{
+  public string username;
+  public string classCode;
+  public string[] correct;
+  public string[] incorrect;
+
+  public string Stringify()
+  {
+      return JsonUtility.ToJson(this);
+  }
+
+  public static PlayerData Parse(string json)
+  {
+      return JsonUtility.FromJson<PlayerData>(json);
+  }
+}
