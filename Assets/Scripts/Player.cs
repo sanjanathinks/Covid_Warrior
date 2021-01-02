73a7ebe23d628d91f4c0aab3cd81bd73a0c99ffb
Selena Qian <sq22@duke.edu>
2021-01-01 22:30:56 -0500

    add user function works

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,10 +15,10 @@ public class Player : MonoBehaviour
     {
         //_rigidBody2D = GetComponent<Rigidbody2D>();
         _playerData = new PlayerData();
-        _playerData.username = "test";
-        StartCoroutine(Download(_playerData.username, (result) => {
+        _playerData.username = "example";
+        /*StartCoroutine(Download(_playerData.username, result => {
           Debug.Log(result.Stringify());
-        }));
+        }));*/
     }
 
     void Update()
@@ -28,6 +28,20 @@ public class Player : MonoBehaviour
 
     void FixedUpdate() {
         // Physics related updates here ...
+        if (Input.GetKeyDown("space"))
+        {
+          StartCoroutine(Download(_playerData.username, result => {
+            Debug.Log(result);
+            if (result == null) {
+              StartCoroutine(Upload(_playerData.Stringify(), added => {
+                Debug.Log(added);
+              }));
+            }
+            else {
+              Debug.Log("Username already exists. Please select a different username or log in.");
+            }
+          }));
+        }
     }
 
     IEnumerator Download(string id, System.Action<PlayerData> callback = null)
@@ -46,9 +60,42 @@ public class Player : MonoBehaviour
           }
           else
           {
-              if (callback != null)
+            if (request.downloadHandler.text == "null")
+            {
+              callback.Invoke(null);
+            }
+            else if (callback != null)
+            {
+                callback.Invoke(PlayerData.Parse(request.downloadHandler.text));
+            }
+          }
+      }
+  }
+
+  IEnumerator Upload(string profile, System.Action<string> callback = null)
+  {
+    Debug.Log(profile);
+      using (UnityWebRequest request = new UnityWebRequest("https://webhooks.mongodb-realm.com/api/client/v2.0/app/covidwarrior-xhivn/service/CovidWarriorInfo/incoming_webhook/addUser", "POST"))
+      {
+          request.SetRequestHeader("Content-Type", "application/json");
+          byte[] bodyRaw = Encoding.UTF8.GetBytes(profile);
+          request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+          request.downloadHandler = new DownloadHandlerBuffer();
+          yield return request.SendWebRequest();
+
+          if (request.isNetworkError || request.isHttpError)
+          {
+              Debug.Log(request.error);
+              if(callback != null)
               {
-                  callback.Invoke(PlayerData.Parse(request.downloadHandler.text));
+                  callback.Invoke("false");
+              }
+          }
+          else
+          {
+              if(callback != null)
+              {
+                  callback.Invoke(request.downloadHandler.text);
               }
           }
       }
