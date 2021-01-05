efbdde716b4a3645ac6ff064703ed6fcd891fa74
Selena Qian <sq22@duke.edu>
2021-01-05 16:11:59 -0500

    added correct parsing for the ints

--- Assets/Scripts/QuestionData.cs
+++ Assets/Scripts/QuestionData.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[System.Serializable]
 public class QuestionData
 {
   public string id;
@@ -23,9 +24,20 @@ public class QuestionData
   public static QuestionData Parse(string json)
   {
       QuestionData q = JsonUtility.FromJson<QuestionData>(json);
-      int id_start = json.IndexOf("$oid\":\"") + "$oid\":\"".Length;
-      int id_end = json.IndexOf("question") - 4;
-      q.id = json.Substring(id_start, id_end - id_start);
+      int start = json.IndexOf("$oid\":\"") + "$oid\":\"".Length;
+      int end = json.IndexOf("question") - 4;
+      q.id = json.Substring(start, end - start);
+
+      start = json.IndexOf("level\":{\"$numberInt\":\"") + "level\":{\"$numberInt\":\"".Length;
+      end = json.IndexOf("\"},\"correct");
+      Debug.Log(json.Substring(start, end - start));
+      q.level = int.Parse(json.Substring(start, end - start));
+
+      start = json.IndexOf("bridges_unit\":{\"$numberInt\":\"") + "bridges_unit\":{\"$numberInt\":\"".Length;
+      end = json.IndexOf("\"},\"nc_standard");
+      Debug.Log(json.Substring(start, end - start));
+      q.bridges_unit = int.Parse(json.Substring(start, end - start));
+
       return q;
   }
 }
