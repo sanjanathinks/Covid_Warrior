6ee41d71afd74ea23801ed0fb254254149bbb20f
sanjanathinks <sanjanapendharkar01@gmail.com>
2021-01-06 14:16:09 -0500

    added everything back

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerData
+{
+  public string username;
+  public string class_code;
+  public string[] questions_correct;
+  public string[] questions_incorrect;
+  public string progress;
+
+  public string Stringify()
+  {
+      return JsonUtility.ToJson(this);
+  }
+
+  public static PlayerData Parse(string json)
+  {
+      return JsonUtility.FromJson<PlayerData>(json);
+  }
+}
