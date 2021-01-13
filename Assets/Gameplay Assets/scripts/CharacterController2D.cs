7931e2f6b6cf2c5bf6268f228dae5fb66caa1d46
iigindesign <46540908+iigindesign@users.noreply.github.com>
2021-01-12 22:56:02 -0800

    updated crouch control
    - automatic crouching bug fix

--- Assets/Gameplay Assets/scripts/CharacterController2D.cs
+++ Assets/Gameplay Assets/scripts/CharacterController2D.cs
@@ -70,14 +70,14 @@ public class CharacterController2D : MonoBehaviour
 	public void Move(float move, bool crouch, bool jump)
 	{
 		// If crouching, check to see if the character can stand up
-		if (!crouch)
+		/* if (!crouch)
 		{
 			// If the character has a ceiling preventing them from standing up, keep them crouching
 			if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
 			{
 				crouch = true;
 			}
-		}
+		} */
 
 		//only control the player if grounded or airControl is turned on
 		if (m_Grounded || m_AirControl)
