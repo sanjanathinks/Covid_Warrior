e1a0a16b1f59eed0892d16a03cfdc0bad7cd58d9
Selena Qian <sq22@duke.edu>
2021-01-14 11:37:49 -0500

    made attack button visible when in battle, not interactable

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,21 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Monster : MonoBehaviour
 {
     public GameObject main;
     public GameObject virtualCam;
+    public Button attack;
 
     public int health = 10;
+
+    private GameObject player;
+
+    void Start() {
+      player = GameObject.Find("player");
+      attack.interactable = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
       if (health <= 0) {
         virtualCam.SetActive(false);
-        GameObject.Find("player").GetComponent<Player>().updateUser();
-        GameObject.Find("player").GetComponent<PlayerMovement>().setBattle(false);
+        player.GetComponent<Player>().updateUser();
+        player.GetComponent<PlayerMovement>().setBattle(false);
+        attack.interactable = false;
+        attack.gameObject.SetActive(false);
         Destroy(this.gameObject);
       }
     }
@@ -39,6 +50,7 @@ public class Monster : MonoBehaviour
       //note that this also triggers if you have editor window open and you can see the monster
       //but it should be fine for actual gameplay
       virtualCam.SetActive(true);
+      attack.gameObject.SetActive(true);
       GetComponent<MonsterMove>().enabled = true;
     }
 }
