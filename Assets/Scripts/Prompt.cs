b6dbdc91023c60de003f1b998df77fe79e7a321b
Selena Qian <sq22@duke.edu>
2021-01-19 18:51:47 -0500

    fact shows and disappears on key press, also disappears once far enough away

---
+++ Assets/Scripts/Prompt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prompt : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject factBckgd;
+    public GameObject factText;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("e") && factBckgd.activeSelf) {
+          factBckgd.SetActive(false);
+          factText.SetActive(false);
+          canvas.SetActive(false);
+        } else if (Input.GetKeyDown("e")) {
+          factBckgd.SetActive(true);
+          factText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+      if (col.gameObject.name.Equals("player")) {
+        canvas.SetActive(false);
+      }
+    }
+}
--- Assets/Scripts/Treasure_Chest.cs
+++ Assets/Scripts/Treasure_Chest.cs
@@ -18,25 +18,4 @@ public class Treasure_Chest : MonoBehaviour
         canvas.SetActive(true);
       }
     }
-
-    /*
-    private Canvas fun_fact;
-    private bool fun_fact_active = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.E)) {
-            fun_fact_active = !fun_fact_active;
-            fun_fact.transform.SetActive(fun_fact);
-        }
-    }*/
 }
-
-// want window to close with a different key
-// one key to open chest, another key to close pop up
